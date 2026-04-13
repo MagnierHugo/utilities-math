590eaaaaf98c45e1e98d24577fa33af22a7f05b2
MagnierHugo <magnierhugo@proton.me>
2026-04-13 18:27:37 +0200

    feat(common): Add Common Arithmetic utils implementations

--- include/common.hpp
+++ include/common.hpp
@@ -1,22 +1,44 @@
 #pragma once
 
+#include <type_traits>
+
 
 namespace mh::math {
 
 	template<class T>
-	concept ArithmeticType = true;
+	concept ArithmeticType = std::is_arithmetic_v<T>;
 
 	template<class T, T N1>
-	concept NonZero = true;
+	concept NonZero = ArithmeticType<T> && (N1 != T{});
 
 	#define _NonZero(N1) NonZero<decltype(N1), N1>
 
-	template<class T, T N, T Exponent>
+	#pragma region power_of
+
+	template<class T, T N, int Exponent>
 	requires(
 		ArithmeticType<T>
 	)
 	struct power_of {
-		static constexpr inline T value = T{};
+		static constexpr inline T value = N * power_of<T, N, Exponent - 1>::value;
 	};
+
+	template<class T, T N>
+	requires(
+		ArithmeticType<T>
+	)
+	struct power_of<T, N, 1> {
+		static constexpr inline T value = N;
+	};
+
+	template<class T, T N>
+	requires(
+		ArithmeticType<T>
+	)
+	struct power_of<T, N, 0> {
+		static constexpr inline T value = 1;
+	};
+
+	#pragma endregion
 
 }
--- tests/test_common.cpp
+++ tests/test_common.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 
 #include "catch2/catch_test_macros.hpp"
 
@@ -11,38 +12,42 @@ namespace mh::math {
 
 		REQUIRE(ArithmeticType<int>);
 		REQUIRE(ArithmeticType<double>);
+		REQUIRE(ArithmeticType<char>);
 		REQUIRE_FALSE(ArithmeticType<std::string>);
-		REQUIRE_FALSE(ArithmeticType<char>);
+		REQUIRE_FALSE(ArithmeticType<std::vector<int>>);
 
 	}
 
 	TEST_CASE("NonZero concept correctly validates types", "[Common] [Concept]") {
 
-		REQUIRE(NonZero<int, 0>);
-		REQUIRE(NonZero<int, int{}>);
-		REQUIRE(NonZero<size_t, 0>);
-		REQUIRE(NonZero<size_t, size_t{}>);
-		REQUIRE_FALSE(NonZero<int, 1>);
-		REQUIRE_FALSE(NonZero<int, int{ 1 }>);
-		REQUIRE_FALSE(NonZero<size_t, 0xffffffff>);
-		REQUIRE_FALSE(NonZero<size_t, size_t{ 0xffffffff }>);
+		REQUIRE(NonZero<int, 1>);
+		REQUIRE(NonZero<int, int{ 1 }>);
+		REQUIRE(NonZero<size_t, 0xffffffff>);
+		REQUIRE(NonZero<size_t, size_t{ 0xffffffff }>);
+		REQUIRE_FALSE(NonZero<int, 0>);
+		REQUIRE_FALSE(NonZero<int, int{}>);
+		REQUIRE_FALSE(NonZero<size_t, 0>);
+		REQUIRE_FALSE(NonZero<size_t, size_t{}>);
 
 	}
 
 	TEST_CASE("_NonZero macro correctly validates types", "[Common] [Macro]") {
 
-		REQUIRE(_NonZero(0));
-		REQUIRE(_NonZero(int{}));
-		REQUIRE(_NonZero(size_t{}));
-		REQUIRE_FALSE(_NonZero(1));
-		REQUIRE_FALSE(_NonZero(int{ 1 }));
-		REQUIRE_FALSE(_NonZero(0xffffffff));
-		REQUIRE_FALSE(_NonZero(size_t{ 0xffffffff }));
+		REQUIRE(_NonZero(1));
+		REQUIRE(_NonZero(int{ 1 }));
+		REQUIRE(_NonZero(0xffffffff));
+		REQUIRE(_NonZero(size_t{ 0xffffffff }));
+		REQUIRE_FALSE(_NonZero(0));
+		REQUIRE_FALSE(_NonZero(int{}));
+		REQUIRE_FALSE(_NonZero(size_t{}));
 
 	}
 
 	TEST_CASE("power_of concept correctly validates types", "[Common]") {
 
+		REQUIRE(power_of<int, 5, 0>::value == 1);
+		REQUIRE(power_of<int, 1, 6>::value == 1);
+		REQUIRE(power_of<int, 0, 5>::value == 0);
 		REQUIRE(power_of<int, 5, 3>::value == 125);
 		REQUIRE(power_of<size_t, 3, 4>::value == 81);
 		REQUIRE(power_of<size_t, 2, 2>::value == 4);
