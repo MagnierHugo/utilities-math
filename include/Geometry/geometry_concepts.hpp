ac6987c1ecbf0149aa513c624953ffc4fdc561ef
MagnierHugo <magnierhugo@proton.me>
2026-04-13 12:39:07 +0200

    feat(geometry): Add Geometry concepts implementations

--- include/Geometry/geometry_concepts.hpp
+++ include/Geometry/geometry_concepts.hpp
@@ -4,12 +4,22 @@
 namespace mh::math {
 
 	template<class T>
-	concept GeometryPolicy = true;
+	concept GeometryPolicy = requires {
+		typename T::size_type;
+		typename T::geometry_array_type;
+	}&& requires {
+		{ T::rank() } -> std::same_as<typename T::size_type>;
+		{ T::size() } -> std::same_as<typename T::size_type>;
+		{ T::geometry() } -> std::same_as<typename T::geometry_array_type>;
+	}&& requires {
+		std::is_default_constructible_v<T>;
+		std::is_trivially_copyable_v<T>;
+	};
 
 	template<class T>
-	concept _1D = true;
+	concept _1D = GeometryPolicy<T> && (T::rank() == 1);
 
 	template<class T>
-	concept _2D = true;
+	concept _2D = GeometryPolicy<T> && (T::rank() == 2);
 
 }
