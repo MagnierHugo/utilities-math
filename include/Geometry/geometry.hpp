ea38372b563b3195a1b1d8d708217025a369ee57
MagnierHugo <magnierhugo@proton.me>
2026-04-13 10:53:35 +0200

    feat(geometry): Add Geometry class implementation

--- include/Geometry/geometry.hpp
+++ include/Geometry/geometry.hpp
@@ -14,16 +14,16 @@ namespace mh::math {
 		using size_type = size_t;
 		using geometry_array_type = const std::array<size_type, sizeof...(OtherSizes) + 1>;
 
-		static consteval inline size_type size() noexcept {
-			return {};
+		static consteval inline size_type rank() noexcept {
+			return sizeof...(OtherSizes) + 1;
 		}
 
-		static consteval inline size_type rank() noexcept {
-			return {};
+		static consteval inline size_type size() noexcept {
+			return FirstSize * (OtherSizes * ...);
 		}
 
 		static consteval inline geometry_array_type geometry() noexcept {
-			return {};
+			return { FirstSize, OtherSizes... };
 		}
 	};
 
